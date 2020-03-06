41bee0a5a88d872f72d4edd2d3636c39953bde9b
Ash Isbitt <AIsbitt@tcadmins-MacBook-Air-2.local>
2020-03-06 16:01:22 +0000

    overwrite init

--- reptile_class.py
+++ reptile_class.py
@@ -3,9 +3,26 @@ from animal_class import Animal
 
 class Reptile(Animal):
 
+    def __init__(self, name, age, colour, heart, brain, scales, blood):
+        super().__init__(name, age, colour, heart, brain)
+        self.scales = scales
+        self.blood = blood
+
     def make_sound(self):
         return 'ssssss'
 
+    def prey(self):
+        return "Mmm. Dinner"
+
+    def seek_sun(self):
+        return "Warmth"
+
+    def seek_shade(self):
+        return "Cool"
+
 
 animal1 = Animal("Ringo", 12, 'Red', True, True)
-reptile = Reptile("Tom", 15, 'Green', True, True)
+reptile = Reptile("Tom", 15, 'Green', True, True, True, "cold")
+
+print(reptile.scales)
+print(reptile.age)
